3821ea4d81e9f4ca19b8c095842e0d35de0c6254
pkovalski <pricila.kovalski@tools.com.br>
2020-03-29 10:44:46 -0300

    29032020 - Criação do Projeto

---
+++ app/src/main/java/viagens/com/br/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package viagens.com.br.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import viagens.com.br.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
